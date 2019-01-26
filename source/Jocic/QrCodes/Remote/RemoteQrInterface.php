f6321a5393428ec07ecf10139c6e8a3403224aaf
Đorđe Jocić <office@djordjejocic.com>
2019-01-26 18:51:16 +0100

    Added interface for Remote QR code creation.

---
+++ source/Jocic/QrCodes/Remote/RemoteQrInterface.php
@@ -0,0 +1,109 @@
+<?php
+    
+    /*******************************************************************\
+    |* Author: Djordje Jocic                                           *|
+    |* Year: 2019                                                      *|
+    |* License: MIT License (MIT)                                      *|
+    |* =============================================================== *|
+    |* Personal Website: http://www.djordjejocic.com/                  *|
+    |* =============================================================== *|
+    |* Permission is hereby granted, free of charge, to any person     *|
+    |* obtaining a copy of this software and associated documentation  *|
+    |* files (the "Software"), to deal in the Software without         *|
+    |* restriction, including without limitation the rights to use,    *|
+    |* copy, modify, merge, publish, distribute, sublicense, and/or    *|
+    |* sell copies of the Software, and to permit persons to whom the  *|
+    |* Software is furnished to do so, subject to the following        *|
+    |* conditions.                                                     *|
+    |* --------------------------------------------------------------- *|
+    |* The above copyright notice and this permission notice shall be  *|
+    |* included in all copies or substantial portions of the Software. *|
+    |* --------------------------------------------------------------- *|
+    |* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, *|
+    |* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES *|
+    |* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND        *|
+    |* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT     *|
+    |* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,    *|
+    |* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, RISING     *|
+    |* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR   *|
+    |* OTHER DEALINGS IN THE SOFTWARE.                                 *|
+    \*******************************************************************/
+    
+    namespace Jocic\GoogleAuthenticator\Qr\Remote;
+    
+    /**
+     * <i>RemoteQrInterface</i> is an interface used to enforce implementation of
+     * additional qr's methods used by remote QR generators.
+     * 
+     * @author    Djordje Jocic <office@djordjejocic.com>
+     * @copyright 2019 All Rights Reserved
+     * @version   1.0.0
+     */
+    
+    interface RemoteQrInterface
+    {
+        /***************\
+        |* GET METHODS *|
+        \***************/
+        
+        /**
+         * Returns set API key for the remote QR code generator.
+         * 
+         * @author    Djordje Jocic <office@djordjejocic.com>
+         * @copyright 2018 All Rights Reserved
+         * @version   1.0.0
+         */
+        
+        public function getApiKey();
+        
+        /**
+          * Forms and returns an appropriate URL for that can be used for
+         * generating QR codes remotely by sending a GET request.
+         * 
+         * @author    Djordje Jocic <office@djordjejocic.com>
+         * @copyright 2019 All Rights Reserved
+         * @version   1.0.0
+         * 
+         * @param string $value
+         *   Value that should be used for generating the QR code.
+         */
+        
+        public function getUrl($value);
+        
+        /***************\
+        |* SET METHODS *|
+        \***************/
+        
+        // SET METHODS GO HERE
+        
+        /****************\
+        |* CORE METHODS *|
+        \****************/
+        
+        /**
+         * Sets API key for the remote QR code generator.
+         * 
+         * @author    Djordje Jocic <office@djordjejocic.com>
+         * @copyright 2019 All Rights Reserved
+         * @version   1.0.0
+         * 
+         * @param string $apiKey
+         *   API key that should be used.
+         */
+        
+        public function setApiKey($apiKey);
+        
+        /*****************\
+        |* CHECK METHODS *|
+        \*****************/
+        
+        // CHECK METHODS GO HERE
+        
+        /*****************\
+        |* OTHER METHODS *|
+        \*****************/
+        
+        // OTHER METHODS GO HERE
+    }
+    
+?>
